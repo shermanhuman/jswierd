a4d54baac057d9bb790bd0875dcec7cf7eb6f7d8
seriousmumbo <sherman.boyd@gmail.com>
2015-11-06 15:38:39 -0700

    comments

--- L36_by_value_vs_by_reference/property-to-property.js
+++ L36_by_value_vs_by_reference/property-to-property.js
@@ -1,3 +1,6 @@
+//  When you set an object property to another object property 
+//  is it by value or referential?
+
 var dude = { man: 0, dudeman: 1 };
 dude.man = dude.dudeman;
 dude.dudeman = 44;
