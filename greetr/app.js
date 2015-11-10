dec8c77b7b3925499c1a9f5bb6c38b9478de8a10
Sherman Boyd <sherman.boyd@gmail.com>
2015-11-09 17:45:43 -0700

    Not sure.

--- greetr/app.js
+++ greetr/app.js
@@ -1,12 +1,13 @@
 (function(global, $){
     var Greetr = function(firstname, lastname, language) {
-        return new init(firstname, lastname, language);
+        return new Greetr.init(firstname, lastname, language);
     }
-}(window, null));
 
-var init = function(firstname, lastname, language) {
-    console.log(this);
-    return this;
-}
+    Greetr.init = function(firstname, lastname, language) {
+        var self = this;
+        self.firstname = firstname || '';
+        self.lastname = lastname || '':
+        self.language = language || 'en';
+    }
 
-Greetr();
+}(window, jQuery));
